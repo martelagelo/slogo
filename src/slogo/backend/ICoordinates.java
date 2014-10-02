57b77e93cee03576069deca84a2bd4880d876572
Michael Ren <michael.ren@duke.edu>
2014-10-02 18:58:37 -0400

    add coordinates, direcitions, lines

--- src/slogo/backend/ICoordinates.java
+++ src/slogo/backend/ICoordinates.java
@@ -1,5 +1,20 @@
 package slogo.backend;
 
+/**
+ * A set of 2D coordinate points
+ *
+ */
 public interface ICoordinates {
-
+	/**
+	 * Get the x coordinate
+	 * 
+	 * @return the x coordinate
+	 */
+	public Number getX();
+	/**
+	 * Get the y coordinate
+	 * 
+	 * @return the y coordinate
+	 */
+	public Number getY();
 }
