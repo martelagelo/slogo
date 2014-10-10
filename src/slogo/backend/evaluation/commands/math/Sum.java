dfc1c241b2fec86911bd108ef58e3f622bc3bedc
Eric Chen <xc58@duke.edu>
2014-10-09 23:24:40 -0400

    ExecutionContext added

--- src/slogo/backend/evaluation/commands/math/Sum.java
+++ src/slogo/backend/evaluation/commands/math/Sum.java
@@ -1,5 +1,20 @@
 package slogo.backend.evaluation.commands.math;
 
-public class Sum {
+import java.util.List;
+
+import slogo.backend.evaluation.IExecutionContext;
+import slogo.backend.evaluation.IOperation;
+
+public class Sum implements IOperation {
+
+    @Override
+    public IExecutionContext execute (List<IExecutionContext> args) {
+        // TODO Auto-generated method stub
+        for (IExecutionContext context: args){
+            String argument = context.environment().get("returnValue");
+            
+        }
+        return null;
+    }
 
 }
