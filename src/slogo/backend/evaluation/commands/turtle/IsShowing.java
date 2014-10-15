17cca3b2356e0a51d6faeecb657f99de722b62e8
Eric Chen <xc58@duke.edu>
2014-10-15 02:31:23 -0400

    several commands implemented

--- src/slogo/backend/evaluation/commands/turtle/IsShowing.java
+++ src/slogo/backend/evaluation/commands/turtle/IsShowing.java
@@ -1,5 +1,23 @@
 package slogo.backend.evaluation.commands.turtle;
 
-public class IsShowing {
+import java.util.List;
+
+import slogo.backend.evaluation.ExecutionContext;
+import slogo.backend.evaluation.IExecutionContext;
+import slogo.backend.evaluation.IOperation;
+import slogo.backend.util.ITurtleStatus;
+import slogo.backend.util.Visibility;
+
+public class IsShowing implements IOperation{
+
+    @Override
+    public IExecutionContext execute (List<IExecutionContext> args) {
+        // TODO Auto-generated method stub
+        ITurtleStatus status = args.get(0).turtles().get("1");
+        Visibility vis = status.turtleVisibility();
+        String returnString = vis.equals(Visibility.VISIBLE)? "1":"0";
+        args.get(0).environment().put("returnValue", returnString);
+        return new ExecutionContext(args.get(0).turtles(),args.get(0).environment());
+    }
 
 }
--- src/slogo/backend/evaluation/commands/turtle/PenUp.java
+++ src/slogo/backend/evaluation/commands/turtle/PenUp.java
@@ -1,5 +1,32 @@
 package slogo.backend.evaluation.commands.turtle;
 
-public class PenUp {
+import java.util.List;
+
+import slogo.backend.evaluation.ExecutionContext;
+import slogo.backend.evaluation.IExecutionContext;
+import slogo.backend.evaluation.IOperation;
+import slogo.backend.util.ICoordinates;
+import slogo.backend.util.IDirection;
+import slogo.backend.util.ITurtleStatus;
+import slogo.backend.util.PenState;
+import slogo.backend.util.TurtleStatus;
+
+public class PenUp implements IOperation{
+
+    @Override
+    public IExecutionContext execute (List<IExecutionContext> args) {
+        // TODO Auto-generated method stub
+        ITurtleStatus status = args.get(0).turtles().get("1");
+        
+        ICoordinates pos = status.turtlePosition();
+        IDirection dir = status.turtleDirection();
+        args.get(0).environment().put("returnValue", "0");
+        
+        
+        
+        ITurtleStatus newStatus = new TurtleStatus(status.lineSequence(),pos,dir,PenState.UP,status.turtleVisibility());
+        args.get(0).turtles().put("1", newStatus);
+        return new ExecutionContext(args.get(0).turtles(),args.get(0).environment());
+    }
 
 }
