c2d4252ae606616cc20975c344702cf1fecf4dea
Michael Ren <michael.ren@duke.edu>
2014-10-20 16:38:46 -0400

    add builder for TurtleStatus and initialize turtles in Backend

--- src/slogo/backend/impl/util/Direction.java
+++ src/slogo/backend/impl/util/Direction.java
@@ -3,20 +3,20 @@ package slogo.backend.impl.util;
 import slogo.backend.util.IDirection;
 
 public class Direction implements IDirection {
-private double direction;
-public Direction(double myDirection){
-    direction = myDirection;
-}
-    @Override
-    public double toDegrees () {
-        // TODO Auto-generated method stub
-        return direction;
-    }
+	private double direction;
+	public Direction(double myDirection){
+		direction = myDirection;
+	}
+	@Override
+	public double toDegrees () {
+		// TODO Auto-generated method stub
+		return direction;
+	}
 
-    @Override
-    public double toRadians () {
-        // TODO Auto-generated method stub
-        return Math.toRadians(direction);
-    }
+	@Override
+	public double toRadians () {
+		// TODO Auto-generated method stub
+		return Math.toRadians(direction);
+	}
 
 }
