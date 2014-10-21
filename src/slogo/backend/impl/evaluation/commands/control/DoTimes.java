e4dbea7a19aebe948bff27f82fb4cf5160195021
Eric Chen <xc58@duke.edu>
2014-10-20 23:12:39 -0400

    DoTimes implemented

--- src/slogo/backend/impl/evaluation/commands/control/DoTimes.java
+++ src/slogo/backend/impl/evaluation/commands/control/DoTimes.java
@@ -1,5 +1,39 @@
 package slogo.backend.impl.evaluation.commands.control;
 
-public class DoTimes {
+import java.util.List;
+
+import slogo.backend.evaluation.IExecutionContext;
+import slogo.backend.evaluation.MalformedSyntaxException;
+import slogo.backend.impl.evaluation.Evaluator;
+import slogo.backend.impl.evaluation.commands.Operation;
+import slogo.backend.parsing.ISyntaxNode;
+
+public class DoTimes extends Operation{
+
+    public DoTimes (String type, int argMin, int argMax) {
+        super(type, 3, 3);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected IExecutionContext executeRaw (List<IExecutionContext> args,
+            IExecutionContext previous, ISyntaxNode current) {
+        // TODO Auto-generated method stub
+        String expression = args.get(1).environment().get("returnValue");
+        String variable = args.get(0).environment().get("returnValue");
+        IExecutionContext context = args.get(2);
+        int number = Integer.parseInt(expression);
+        Evaluator e = new Evaluator();
+        for(int i = 1; i<=number; i++){
+            context.environment().put(variable, String.valueOf(i));
+            try {
+                context = e.evaluate(current.children().get(2), context);
+            } catch (MalformedSyntaxException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        }
+        return context;
+    }
 
 }
