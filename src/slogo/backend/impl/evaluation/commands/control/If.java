1e88438c5fb07bd5ec72d5870d928b3c5a0a9198
Eric Chen <xc58@duke.edu>
2014-10-20 22:22:25 -0400

    If implemented

--- src/slogo/backend/impl/evaluation/commands/control/If.java
+++ src/slogo/backend/impl/evaluation/commands/control/If.java
@@ -1,5 +1,27 @@
 package slogo.backend.impl.evaluation.commands.control;
 
-public class If {
+import java.util.List;
+
+import slogo.backend.evaluation.IExecutionContext;
+import slogo.backend.impl.evaluation.commands.Operation;
+import slogo.backend.parsing.ISyntaxNode;
+
+public class If extends Operation{
+
+    public If (String type, int argMin, int argMax) {
+        super(type, 2, 2);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected IExecutionContext executeRaw (List<IExecutionContext> args,
+            IExecutionContext previous, ISyntaxNode current) {
+        // TODO Auto-generated method stub
+        String argumentTwo = args.get(1).environment().get("returnValue");
+        if(argumentTwo.equals("0")){
+            return previous;
+        }
+        return args.get(0);
+    }
 
 }
