3f976d5d30007df251b58c51b8c11334b811c0c9
nickwidmaier <nickwidmaier@gmail.com>
2014-10-21 22:33:08 -0400

    Color functionality and slight turtle movement fix

--- src/slogo/UI/Turtle.java
+++ src/slogo/UI/Turtle.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class Turtle {
 
@@ -17,6 +18,7 @@ public class Turtle {
 	private boolean myBold;
 	private double orientation;
 	private Map<String, Node> imagesMap;
+	private Color color;
 	//private int turtleID;
 
 	private boolean active;
@@ -30,6 +32,7 @@ public class Turtle {
 		turtleImage = imagesMap.get(s);
 		moveTurtle(xpos, ypos);
 		active = true;
+		color = Color.BLACK;
 		setOrientation(90);
 		activateTurtle();
 	}
@@ -109,7 +112,7 @@ public class Turtle {
 	protected void setImage(String name){
 		turtleImage = imagesMap.get(name); 
 		turtleImage.setRotate(orientation);
-		moveTurtle(xpos+AppConstants.MAX_NEW_IMAGE_WIDTH, ypos + AppConstants.MAX_NEW_IMAGE_HEIGHT);
+		moveTurtle(xpos+AppConstants.MAX_NEW_IMAGE_WIDTH/2, ypos + AppConstants.MAX_NEW_IMAGE_HEIGHT/2);
 	}
 
 	protected Node getImage(){
@@ -147,6 +150,16 @@ public class Turtle {
 	public boolean isActive () {
 		return active;
 	}
+
+
+    public void setColor (Color value) {
+        color = value;
+        
+    }
+    
+    public Color getColor(){
+        return color;
+    }
 	
 
 }
