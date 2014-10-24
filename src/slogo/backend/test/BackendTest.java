7ed45bdbb83313142da43db062a3b6d563145a90
Michael Ren <michael.ren@duke.edu>
2014-10-24 17:52:56 -0400

    add tests for control commands

--- src/slogo/backend/test/BackendTest.java
+++ src/slogo/backend/test/BackendTest.java
@@ -2,6 +2,7 @@ package slogo.backend.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import slogo.Constants;
@@ -12,15 +13,13 @@ import slogo.backend.impl.Backend;
 import slogo.backend.impl.InitializationException;
 
 public class BackendTest {
+	private IModel backend;
+	@Before
+	public void init() throws InitializationException{
+		this.backend = new Backend();
+	}
 	@Test
 	public void testExecute() throws ExecutionException{
-		IModel backend = null;
-		try {
-			backend = new Backend();
-		} catch (InitializationException e) {
-			e.printStackTrace();
-			fail();
-		}
 		IExecutionContext result = backend.execute("Minus Difference 80 50");
 		assertEquals("-30.0", result.environment().get(Constants.RETURN_VALUE_ENVIRONMENT));
 		IExecutionContext foo = backend.execute("Forward 50");
@@ -31,7 +30,42 @@ public class BackendTest {
 	}
 	@Test(expected = ExecutionException.class)
 	public void testExecuteNegative() throws Exception {
-		IModel backend = new Backend();
 		backend.execute("!!@#!@$!@#$@#$");
+	}
+	@Test
+	public void testMake() throws Exception {
+		IExecutionContext result = backend.execute("Make :foo bar");
+		assertEquals("bar", result.environment().get("foo"));
+	}
+	@Test
+	public void testRepeat() throws Exception {
+		IExecutionContext result = backend.execute("Repeat 3 [ Sum 1 1 ]");
+		assertEquals("6.0", result.environment().get(Constants.RETURN_VALUE_ENVIRONMENT));
+	}
+	@Test
+	public void testDoTimes() throws Exception {
+		IExecutionContext result = backend.execute("DoTimes [ :foo 3 ] [ Sum :foo :foo]");
+		assertEquals("12.0", result.environment().get(Constants.RETURN_VALUE_ENVIRONMENT));
+	}
+	@Test
+	public void testFor() throws Exception {
+		IExecutionContext result = backend.execute("For [ :foo 3 6 2 ] [ Sum :foo :foo ]");
+		assertEquals("16.0", result.environment().get(Constants.RETURN_VALUE_ENVIRONMENT));
+	}
+	@Test
+	public void testIf() throws Exception {
+		IExecutionContext result = backend.execute("If 0 [ Minus 1 ]");
+		assertNotEquals("-1.0", result.environment().get(Constants.RETURN_VALUE_ENVIRONMENT));
+	}
+	@Test
+	public void testIfElse() throws Exception {
+		IExecutionContext result = backend.execute("IfElse 0 [ Minus 1 ] [ Minus 2 ]");
+		assertEquals("-2.0", result.environment().get(Constants.RETURN_VALUE_ENVIRONMENT));
+	}
+	@Test
+	public void testTo() throws Exception {
+		backend.execute("To foo [ :bar ] [ Minus :bar ]");
+		IExecutionContext result = backend.execute("foo 1");
+		assertEquals("-1.0", result.environment().get(Constants.RETURN_VALUE_ENVIRONMENT));
 	}
 }
