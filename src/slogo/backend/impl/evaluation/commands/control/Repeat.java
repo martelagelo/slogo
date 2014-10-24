c0b99cd2e8944ace5aa3ba535db18a20249533a6
Eric Chen <xc58@duke.edu>
2014-10-24 17:39:11 -0400

    Repeat supports list of commands

--- src/slogo/backend/impl/evaluation/commands/control/Repeat.java
+++ src/slogo/backend/impl/evaluation/commands/control/Repeat.java
@@ -2,9 +2,11 @@ package slogo.backend.impl.evaluation.commands.control;
 
 import java.util.List;
 
+import slogo.Constants;
 import slogo.backend.evaluation.IExecutionContext;
 import slogo.backend.evaluation.MalformedSyntaxException;
 import slogo.backend.impl.evaluation.Evaluator;
+import slogo.backend.impl.evaluation.ExecutionContext;
 import slogo.backend.impl.evaluation.commands.Operation;
 import slogo.backend.parsing.ISyntaxNode;
 
@@ -18,20 +20,24 @@ public class Repeat extends Operation{
     @Override
     protected IExecutionContext executeRaw (List<IExecutionContext> args, IExecutionContext previous, ISyntaxNode current) {
         // TODO Auto-generated method stub
-        String expression = args.get(0).environment().get("returnValue");
+        String expression = args.get(0).environment().get(Constants.RETURN_VALUE_ENVIRONMENT);
         int times = Integer.parseInt(expression);
         //Error check times > 1
         Evaluator e = new Evaluator();
-        IExecutionContext context = args.get(1);
+        IExecutionContext context = previous;
         for (int i = 0 ; i<times-1; i++){
             try {
-                context = e.evaluate(current.children().get(1), context);
+                for(int k = 2; k < args.size()-1; k++){
+                    
+                
+                context = e.evaluate(current.children().get(k), context);
+                }
             } catch (MalformedSyntaxException e1) {
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
             }
         }
-        return context;
+        return new ExecutionContext(context.turtles(),context.environment(),context.userDefinedCommands());
     }
 
 }
