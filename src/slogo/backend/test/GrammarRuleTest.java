afbfa7818084e83a710558085e2d281eb60cb278
Michael Ren <michael.ren@duke.edu>
2014-10-24 00:31:25 -0400

    refactor and comment wildcard matching; refactor wildcard matching tests

--- src/slogo/backend/test/GrammarRuleTest.java
+++ src/slogo/backend/test/GrammarRuleTest.java
@@ -10,35 +10,46 @@ import slogo.Constants;
 import slogo.backend.impl.parsing.GrammarRule;
 
 public class GrammarRuleTest {
-	//@Test
 	public void testInfiniteMatchRecurse(boolean expected, String[] pattern, String[] search){
 		boolean result = GrammarRule.infiniteMatchRecurse(Arrays.asList(pattern),
 				Arrays.asList(search));
 		assertEquals(expected, result);
 	}
 	@Test
-	public void testInfiniteMatch(){
+	public void testSunny(){
 		boolean sunnyResult = true;
 		String[][] sunny = {
 				{"hi", "there", Constants.INFINITE_MATCHING_LABEL, "foo" },
 				{ "hi", "there", "there", "foo" }};
 		testInfiniteMatchRecurse(sunnyResult, sunny[0], sunny[1]);
+	}
+	@Test
+	public void testSunnyEnd(){
 		boolean sunnyEndResult = true;
 		String[][] sunnyEnd = {
 				{"hi", "there", Constants.INFINITE_MATCHING_LABEL },
 				{ "hi", "there", "there", "there", "there" }};
 		testInfiniteMatchRecurse(sunnyEndResult, sunnyEnd[0], sunnyEnd[1]);
+	}
+	@Test
+	public void testExtraPattern(){
 		boolean extraPatternResult = false;
 		String[][] extraPattern = {
 				{"hi", "there", Constants.INFINITE_MATCHING_LABEL, "blah" },
 				{ "hi", "there", "there", "there" }
 		};
 		testInfiniteMatchRecurse(extraPatternResult, extraPattern[0], extraPattern[1]);
+	}
+	@Test
+	public void testExtraSearch(){
 		boolean extraSearchResult = false;
 		String[][] extraSearch = {
 				{"hi", "there", Constants.INFINITE_MATCHING_LABEL },
 				{"hi", "there", "there", "blah" }};
 		testInfiniteMatchRecurse(extraSearchResult, extraSearch[0], extraSearch[1]);
+	}
+	@Test
+	public void testJustOneRepeat(){
 		boolean justOneRepeatResult = true;
 		String[][] justOneRepeat = {
 				{"hi", "there", Constants.INFINITE_MATCHING_LABEL },
