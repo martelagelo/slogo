4e4a2c37b9e4994b6edb50708664bd6865cd4706
Eric Chen <xc58@duke.edu>
2014-10-25 19:01:15 -0400

    back now supports multiturtle

--- src/slogo/backend/impl/evaluation/commands/turtle/Back.java
+++ src/slogo/backend/impl/evaluation/commands/turtle/Back.java
@@ -1,6 +1,7 @@
 package slogo.backend.impl.evaluation.commands.turtle;
 
 import java.util.List;
+import java.util.Map;
 
 import slogo.Constants;
 import slogo.backend.evaluation.IExecutionContext;
@@ -24,29 +25,36 @@ public class Back extends Operation{
     }
 
     @Override
-	protected IExecutionContext executeRaw (List<IExecutionContext> args, IExecutionContext previous, ISyntaxNode current) {
-        ITurtleStatus status = args.get(0).turtles().get(Constants.DEFAULT_TURTLE_NAME);
+    protected IExecutionContext executeRaw (List<IExecutionContext> args, IExecutionContext previous, ISyntaxNode current) {
         String backward = args.get(0).environment().get(Constants.RETURN_VALUE_ENVIRONMENT);
         double backwardValue = Double.parseDouble(backward);
-        ICoordinates pos = status.turtlePosition();
-        IDirection dir = status.turtleDirection();
-        PenState pen = status.penState();
-        Visibility vis = Visibility.VISIBLE;
-      
-        if(pen.equals(PenState.UP)){
-            vis = Visibility.INVISIBLE;
+        Map <String,ITurtleStatus> turtles = args.get(0).turtles();
+
+        for(String name: turtles.keySet()){
+
+            ITurtleStatus status = args.get(0).turtles().get(name);
+            if(status.isActive()){
+                ICoordinates pos = status.turtlePosition();
+                IDirection dir = status.turtleDirection();
+                PenState pen = status.penState();
+                Visibility vis = Visibility.VISIBLE;
+
+                if(pen.equals(PenState.UP)){
+                    vis = Visibility.INVISIBLE;
+                }
+                double newX = pos.getX().doubleValue()- backwardValue*Math.cos(dir.toRadians());
+                double newY = pos.getY().doubleValue()- backwardValue*Math.sin(dir.toRadians());
+                ICoordinates newPos = new Coordinates(newX,newY);
+
+                ILine newLine = new Line(pos, newPos, vis);
+                status.lineSequence().add(newLine);
+                ITurtleStatus newStatus = new TurtleStatus(status.lineSequence(),newPos,dir,pen,status.turtleVisibility(), status.turtleQualities());
+                turtles.put(name, newStatus);
+            }
         }
-        double newX = pos.getX().doubleValue()- backwardValue*Math.cos(dir.toRadians());
-        double newY = pos.getY().doubleValue()- backwardValue*Math.sin(dir.toRadians());
-        ICoordinates newPos = new Coordinates(newX,newY);
-        
-        ILine newLine = new Line(pos, newPos, vis);
-        status.lineSequence().add(newLine);
-        ITurtleStatus newStatus = new TurtleStatus(status.lineSequence(),newPos,dir,pen,status.turtleVisibility(), status.turtleQualities());
-        args.get(0).turtles().put(Constants.DEFAULT_TURTLE_NAME, newStatus);
         return new ExecutionContext(args.get(0).turtles(),args.get(0).environment(), args.get(0).userDefinedCommands());
     }
 
-   
+
 
 }
