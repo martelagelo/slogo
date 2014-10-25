287d67e3c44044a67fcfa4c1a8eaa35eb3f190f5
Michael Ren <michael.ren@duke.edu>
2014-10-25 15:18:09 -0400

    fix typo in command regex

--- src/slogo/backend/impl/Backend.java
+++ src/slogo/backend/impl/Backend.java
@@ -59,7 +59,7 @@ public class Backend implements IModel{
 	private List<ITokenRule> tokenRules(){
 		List<ITokenRule> rules = new ArrayList<>();
 		rules.add(new TokenRule.Builder(Constants.CONSTANT_LABEL, "-?[0-9]+\\.?[0-9]*").build());
-		rules.add(new TokenRule.Builder(Constants.COMMAND_LABEL, "[a-zA-z_]+(\\?)?").build());
+		rules.add(new TokenRule.Builder(Constants.COMMAND_LABEL, "[a-zA-Z_]+(\\?)?").build());
 		rules.add(new TokenRule.Builder(Constants.VARIABLE_LABEL, ":[a-zA-Z]+").build());
 		rules.add(new TokenRule.Builder(Constants.OPENING_LIST_LABEL, "\\[").build());
 		rules.add(new TokenRule.Builder(Constants.CLOSING_LIST_LABEL, "\\]").build());
