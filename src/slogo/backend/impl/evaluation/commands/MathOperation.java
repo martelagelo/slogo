86adeb973f0e96fdf3f4481f6ac1fcc5796be5a1
Michael Ren <michael.ren@duke.edu>
2014-10-25 23:13:17 -0400

    Catch divide by zero

--- src/slogo/backend/impl/evaluation/commands/MathOperation.java
+++ src/slogo/backend/impl/evaluation/commands/MathOperation.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import slogo.Constants;
 import slogo.backend.evaluation.IExecutionContext;
+import slogo.backend.evaluation.MalformedSyntaxException;
 import slogo.backend.impl.evaluation.ExecutionContext;
 import slogo.backend.parsing.ISyntaxNode;
 
@@ -17,12 +18,12 @@ public abstract class MathOperation extends Operation{
 	}
 
 	@Override
-	protected IExecutionContext executeRaw(List<IExecutionContext> args, IExecutionContext previous, ISyntaxNode current) {
+	protected IExecutionContext executeRaw(List<IExecutionContext> args, IExecutionContext previous, ISyntaxNode current) throws MalformedSyntaxException {
         List<Number> argsNum = generateNumericArguments(args);
         String result = executeMath(argsNum).toString();
         IExecutionContext retContext = mergeContexts(args);
         retContext.environment().put(Constants.RETURN_VALUE_ENVIRONMENT, result);
         return retContext;
 	}
-	protected abstract Number executeMath(List<Number> args);
+	protected abstract Number executeMath(List<Number> args) throws MalformedSyntaxException;
 }
